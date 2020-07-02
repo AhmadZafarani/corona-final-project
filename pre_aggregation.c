bb0e26efed1f4a9e16a095b9691f9353580f94d1
ahmad <ahmadzafarani2013@gmail.com>
2020-07-02 16:45:50 +0430

    protocol of connection to database updated, reader.c reformatted

--- pre_aggregation.c
+++ pre_aggregation.c
@@ -1,3 +1,4 @@
+// YA MASOOMEH
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +16,8 @@ int main() {
 
     PGconn *conn = PQconnectdb("user=postgres dbname=fpdb");
     if (PQstatus(conn) == CONNECTION_BAD && strstr(PQerrorMessage(conn), "not exist") != NULL) {
+    /*  if fpdb does not exists, server will raise an error with this message : FATAL : database fpdb not exist
+        in this case, we close the connection, connect to server again, create fpdb database and close the connection again!*/
 
         PQfinish(conn);
         PGconn *conn1 = PQconnectdb("user=postgres");
@@ -26,6 +29,7 @@ int main() {
         PQfinish(conn1);
         PGconn *conn = PQconnectdb("user=postgres dbname=fpdb");
     } else if (PQstatus(conn) == CONNECTION_BAD)    {
+        //unhandled error occured, print error message and close the connection
 
         fprintf(stderr, "Connection to database failed: %s\n", PQerrorMessage(conn));
         PQfinish(conn);
@@ -33,8 +37,14 @@ int main() {
     }
     printf("connection to server was successfull :)\nnow you are connected to fpdb database!\n");
 
-
-    PGresult *res = PQexec(conn, "CREATE TABLE fp_stores_data ( time varchar(50), province varchar(50), city varchar(50), " \
+    /*successfully connected to fpdb database! create the tables with specified columns*/
+    
+    PGresult *res = PQexec(conn, "DROP TABLE IF EXISTS fp_stores_data");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        do_exit(conn, res);     
+    }
+    PQclear(res);
+    res = PQexec(conn, "CREATE TABLE fp_stores_data ( time varchar(50), province varchar(50), city varchar(50), " \
         "market_id int,product_id int, price int, quantity int, has_sold int)");
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
         do_exit(conn, res);     
@@ -44,24 +54,34 @@ int main() {
     PQclear(res);    
 
 
-    res = PQexec(conn, "CREATE TABLE fp_city_aggregation ( city varchar(50), quantity int, has_sold int, price_avg float, " \
-        "PRIMARY KEY (city) )");
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);     
-    }   else    {
-        printf("fp_city_aggregation table created :)\n");
-    }
-    PQclear(res);
+    // res = PQexec(conn, "DROP TABLE IF EXISTS fp_city_aggregation");
+    // if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    //     do_exit(conn, res);     
+    // }
+    // PQclear(res);
+    // res = PQexec(conn, "CREATE TABLE fp_city_aggregation ( city varchar(50), time varchar(50), total_quantity int, " \
+    //     "total_has_sold int, price_avg float, PRIMARY KEY (city, time) )");
+    // if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    //     do_exit(conn, res);     
+    // }   else    {
+    //     printf("fp_city_aggregation table created :)\n");
+    // }
+    // PQclear(res);
 
 
-    res = PQexec(conn, "CREATE TABLE fp_store_aggregation ( market_id int, total_has_sold int, total_price int, " \
-        "PRIMARY KEY (market_id) );");
-    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);     
-    }   else    {
-        printf("fp_store_aggregation table created :)\n");
-    }
-    PQclear(res);
+    // res = PQexec(conn, "DROP TABLE IF EXISTS fp_store_aggregation");
+    // if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    //     do_exit(conn, res);     
+    // }
+    // PQclear(res);
+    // res = PQexec(conn, "CREATE TABLE fp_store_aggregation ( market_id int, total_has_sold int, total_price int, " \
+    //     "PRIMARY KEY (market_id) );");
+    // if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+    //     do_exit(conn, res);     
+    // }   else    {
+    //     printf("fp_store_aggregation table created :)\n");
+    // }
+    // PQclear(res);
 
 
     PQfinish(conn);
--- reader.c
+++ reader.c
@@ -3,41 +3,194 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <libpq-fe.h>
+#include <regex.h>
+
 
 const char * directory = "/tmp/final_project/";
 
-void insert_into_stores(char* line) {
+
+char *trim_white_space(char *str) {
+    char *end;
+    while((unsigned char)*str == ' ')
+        str++;
+    if(*str == 0)
+        return str;
+    end = str + strlen(str) - 1;
+    while(end > str && ((unsigned char)*end) == ' ')
+        end--;
+    end[1] = '\0';
+    return str;
 }
 
-int main(void) { 
-	struct dirent *de;
+
+void print_result(int return_value) {
+    if (return_value == 0) {
+        printf("Pattern found.\n");
+    }   else if (return_value == _REG_NOMATCH) {
+        printf("Pattern not found.\n");
+    }   else {
+        printf("An error occured.\n");
+    }
+}
+
+
+int line_to_command(char *line, char *command) {
+    strcpy(command, "INSERT INTO fp_stores_data (time, province, city, market_id, product_id, price, quantity, " \
+        "has_sold) VALUES (");
+    int data_index = 1;
+    char * token = strtok(line, ",");
+
+    while (token != NULL) {
+
+        // if (data_index == 1) {
+        //     strcat(command, "'");
+        //     regex_t regex;
+        //     int compile = regcomp(&regex, "^(?:(?:([01]?\\d|2[0-3]):)?([0-5]?\\d):)?([0-5]?\\d)$", 0);
+        //     if (compile != 0)   {
+        //         printf("regex compile error\n");
+        //     }
+        //     int return_value = regexec(&regex, token, 0, NULL, 0);
+        //     print_result(return_value);
+        //     // if (return_value != 0)  {
+        //     //     return NULL;
+        //     // }   else    {
+        //     //     strcat(command, token);
+        //     // } 
+        //     strcat(command, "'");
+        // }
+
+    //     }   else if (data_index == 2 || data_index == 3) {
+    //         strcat(command, "'");
+    //         regex_t regex;
+    //         regcomp(&regex, "^[A-Za-z]+$", 0);
+    //         int return_value = regexec(&regex, token, 0, NULL, 0);
+    //         print_result(return_value);
+    //         strcat(command, "'");
+
+    //     }   else {
+    //         regex_t regex;
+    //         regcomp(&regex, "^\\d+$", 0);
+    //         int return_value = regexec(&regex, token, 0, NULL, 0);
+    //         print_result(return_value);
+    //     }
+        
+        if (data_index <= 3)    {
+            strcat(command, "'");
+        }
+        token = trim_white_space(token);
+        strcat(command, token);
+        if (data_index <= 3)    {
+            strcat(command, "'");
+        }
+        if (data_index < 8) {
+            strcat(command, ", ");
+        }   else if ( data_index > 8)   {
+            return 0;
+        }
+        token = strtok(NULL, ",");
+        data_index++;
+    }
+    strcat(command, ")");
+    // printf("%s\n", command);
+    return data_index == 9;
+}
+
+
+void execute_query(const char* query, PGconn *conn) {
+    PGresult *res = PQexec(conn, query);
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        fprintf(stderr, "%s\n", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        exit(1);
+    }
+    PQclear(res);  
+}
+
+
+DIR *open_directory(struct dirent *de)   {
 	DIR *dr = opendir(directory);   // set a pointer to directory and open it
 	if (dr == NULL) {
 		printf("Could not open %s\n", directory); 
-		return 0;
+		exit(1);
 	}
+    printf("directory opened successfully!\n");
+    return dr;
+}
 
-	while ((de = readdir(dr)) != NULL && de->d_type != DT_DIR) {    // read all "files" in this directory, line-by-line
+
+PGconn *connect_to_database()   {
+    PGconn *conn = PQconnectdb("user=postgres dbname=fpdb");
+    if (PQstatus(conn) == CONNECTION_BAD) {
+        fprintf(stderr, "Connection to database failed: %s\n", PQerrorMessage(conn));
+        PQfinish(conn);
+        exit(1);
+    }
+    return conn;
+}
+
+
+int read_file(FILE *file, PGconn *conn) {
+    int is_text_file = 0;       //  if the file isn't a text file, continue
+    char line[1000];
+    while (fgets(line, sizeof(line), file)) {   //  step 1
+        is_text_file = 1;
+
+        int l = strlen(line);
+        line[l-1] = '\0';       //  remove last '\n' from the read line
+
+        char command[1000];
+        int valid = line_to_command(line, command);
+        if (valid)    {
+            execute_query(command, conn);
+        }
+    }
+    return is_text_file;
+}
+
+
+void delete_file(char *name)  {
+    char command[50];       // delete the files after reading them
+    strcpy(command, "rm -f ");
+    strcat(command, name);
+    system(command);
+    printf("file %s deleted!\n", name);
+}
+
+
+int main(void) { 
+	struct dirent *de;
+    DIR *dr = open_directory(de);
+    PGconn *conn = connect_to_database();
+
+    while ((de = readdir(dr)) != NULL) {        // read all files in this directory, line-by-line, and store their data in database
         char name[50];
         strcpy(name, directory);
         strcat(name, de->d_name);
         FILE* file = fopen(name, "r");
-        if (file == NULL)
+        if (file == NULL) {
             printf("Opening file %s failed!\n", name);
-
-        char line[256];
-        while (fgets(line, sizeof(line), file)) {   //  step 1
-            insert_into_stores(line);
+            exit(1);
         }
 
+        if (!read_file(file, conn))   {
+            continue;
+        }
+        printf("file %s opened!\nall VALID data of %s file inserted in fp_stores_data table!\n", name, name);
         fclose(file);
 
-        // char command[50];       // delete the files after reading them
-        // strcpy(command, "rm -f ");
-        // strcat(command, name);
-        // system(command);
+        // delete_file(name);
     }
 
+    execute_query("DROP TABLE IF EXISTS fp_city_aggregation", conn);        //  step 2: aggregations
+    execute_query("CREATE TABLE fp_city_aggregation AS SELECT city, time, SUM(quantity) AS total_quantity, SUM(has_sold) AS " \
+        "total_has_sold FROM fp_stores_data GROUP BY city, time ORDER BY city, time", conn);
+    execute_query("DROP TABLE IF EXISTS fp_store_aggregation", conn);    
+    execute_query("CREATE TABLE fp_store_aggregation AS SELECT market_id, SUM(has_sold) AS total_has_sold, SUM(has_sold * price)" \
+        " AS total_price FROM fp_stores_data GROUP BY market_id", conn);
+
+    PQfinish(conn); 
 	closedir(dr);
 	return 0;
 }
